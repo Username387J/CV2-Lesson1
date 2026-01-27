4200fe3c3b7af9dce1f565ba7c88a0de8c367bae
Username387J <jdeleijagbulu@gmail.com>
2026-01-26 19:49:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+
+#open cv is open computer vision library
+#imread-image read
+# different modes(0=grayscale,1=color-transparency,-1=og)
+
+
+image=cv2.imread("picachu.png",1)
+#like root.title but root shows after 
+cv2.imshow("output",image)
+
+#Below 2 make sure it stays
+cv2.waitKey(0)
+cv2.destroyAllWindows()
